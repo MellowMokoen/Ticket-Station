201f4da5431f1b63c15f8136b6d9955797d7ae9d
Karabo Rithuri <rithurikarabo@gmail.com>
2023-10-18 00:29:14 +0200

    checkout

--- checkout.js
+++ checkout.js
@@ -33,3 +33,55 @@ function methodC(){
     iconC.style.color = "greenyellow";
     cDetails.style.display = "none";
 }
+let cNumber =  document.getElementById('number');
+cNumber.addEventListener('keyup',function(e){
+    let num = cNumber.value;
+    let newValue = '';
+    num = num.replace(/\s/g,'');
+    for(var i=0; i<num.length; i++){
+        if(i%4 == 0 && i>0){
+            newValue = newValue.concat(' ');
+        }
+        newValue = newValue.concat(num[i]);
+        cNumber.value = newValue;
+    }
+    
+
+    let ccNumber = document.getElementById('c-number');
+    if(num.length<16){
+        ccNumber.style.border="1px solid red";
+    }else{
+        ccNumber.style.border="1px solid greenyellow";
+    }
+});
+
+let eDate = document.getElementById('e-date');
+eDate.addEventListener('keyup',function(e){
+    let newInput = eDate.value;
+    if(e.which !== 8){
+        var numChars = e.target.value.length;
+        if(numChars == 2){
+            var thisVal = e.target.value;
+            thisVal += '/';
+            e.target.value = thisVal;
+            console.log(thisVal.length)
+        }
+    }
+
+    if(newInput.length<5){
+        eDate.style.border="1px solid red";
+    }else{
+        eDate.style.border="1px solid greenyellow";
+    }
+})
+
+let cvv = document.getElementById('cvv');
+cvv.addEventListener('keyup',function(e){
+    let elen = cvv.value;
+    let cvvBox = document.getElementById('cvv-box');
+    if(elen.length<3){
+        cvvBox.style.border="1px solid red";
+    }else{
+        cvvBox.style.border="1px solid greenyellow";
+    }
+})
