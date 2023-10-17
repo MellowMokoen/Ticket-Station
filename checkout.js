542e504ec6a45a508e183d9dcfd22e8eda254ce8
Karabo Rithuri <rithurikarabo@gmail.com>
2023-10-17 23:21:22 +0200

    declaring variables

---
+++ checkout.js
@@ -0,0 +1,9 @@
+let tColorA = document.getElementById('tColorA'), 
+tColorB = document.getElementById('tColorB'),
+tColorC = document.getElementById('tColorC'),
+iconA = document.querySelector('.fa-credit-card'),
+iconB = document.querySelector('.fa-building-columns'),
+iconC = document.querySelector('.fa-credit-wallet'), 
+cDetails = document.querySelector('.card-details');
+
+
